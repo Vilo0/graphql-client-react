e45727ba54edbd0c15957f56ab9092054fafc505
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-04-08 23:32:18 -0400

    apollo hooks and apollo client in App and Home

---
+++ src/pages/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery, useLazyQuery } from "@apollo/react-hooks";
+
+const GET_ALL_POST = gql`
+  {
+    allPosts {
+      id
+      title
+      description
+    }
+  }
+`;
+
+const Home = () => {
+  const { data, loading } = useQuery(GET_ALL_POST);
+  const [fetchPosts, { data: posts }] = useLazyQuery(GET_ALL_POST);
+
+  if (loading) return <p className="p-5">Loading...</p>;
+
+  return (
+    <div className="container">
+      <div className="row p-5">
+        {data.allPosts.map((p) => (
+          <div className="col-md-4" key={p.id}>
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">
+                  <h4>{p.title}</h4>
+                </div>
+                <p className="card-text">{p.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="row p-5">
+        <button
+          onClick={() => fetchPosts()}
+          className="btn-btn-raised btn-primary"
+        >
+          Fetch Posts
+        </button>
+      </div>
+      <hr />
+      {JSON.stringify(posts)}
+    </div>
+  );
+};
+
+export default Home;
