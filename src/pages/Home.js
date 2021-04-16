7f53d303e92b94e972831b63b5799142f2245078
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-04-16 09:14:16 -0400

    nav component, register and login page completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { gql } from "apollo-boost";
 import { useQuery, useLazyQuery } from "@apollo/react-hooks";
+import { AuthContext } from '../context/authContext';
+import { useHistory } from 'react-router-dom';
 
 const GET_ALL_POST = gql`
   {
@@ -15,6 +17,17 @@ const GET_ALL_POST = gql`
 const Home = () => {
   const { data, loading } = useQuery(GET_ALL_POST);
   const [fetchPosts, { data: posts }] = useLazyQuery(GET_ALL_POST);
+  // access context
+  const { state, dispatch } = useContext(AuthContext);
+  // react router
+  let history = useHistory();
+
+  const updateUserName = () => {
+    dispatch({
+      type: 'LOGGED_IN_USER',
+      payload: 'Miguel Chamorro'
+    });
+  };
 
   if (loading) return <p className="p-5">Loading...</p>;
 
@@ -44,6 +57,14 @@ const Home = () => {
       </div>
       <hr />
       {JSON.stringify(posts)}
+      <hr />
+      {JSON.stringify(state.user)}
+      <hr />
+      <button className="btn btn-primary" onClick={updateUserName}>
+          Change user name
+      </button>
+      <hr />
+      {JSON.stringify(history)}
     </div>
   );
 };
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,97 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { auth, googleAuthProvider } from "../../firebase";
 
-const Login = () => (
-    <p>Hello am Login!</p>
-);
+const Login = () => {
+  const { dispatch } = useContext(AuthContext);
+  const [email, setEmail] = useState("mchamorro@laescala.cl");
+  const [password, setPassword] = useState("chillan2021");
+  const [loading, setLoading] = useState(false);
+
+  let history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await auth
+        .signInWithEmailAndPassword(email, password)
+        .then(async (result) => {
+          const { user } = result;
+          const idTokenResult = await user.getIdTokenResult();
+
+          dispatch({
+            type: "LOGGED_IN_USER",
+            payload: { email: user.email, token: idTokenResult.token },
+          });
+
+          // send user info to our server with mongodb to either update/create
+
+          history.push("/");
+        });
+    } catch (error) {
+      console.log("login error", error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  const googleLogin = () => {
+    auth.signInWithPopup(googleAuthProvider).then(async (result) => {
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: "LOGGED_iN_USER",
+        payload: { email: user.email, token: idTokenResult.token },
+      });
+      // send user info to our server with mongodb to either update/create
+
+      history.push("/");
+    });
+  };
+
+  return (
+    <div className="container p-5">
+      {loading ? <h4 className="text-danger">Loading...</h4> : <h4>Login</h4>}
+      <button onClick={googleLogin} className="btn btn-raised btn-danger mt-5">
+        Login with Google
+      </button>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            placeholder="Enter email"
+            disabled={loading}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            placeholder="Enter password"
+            disabled={loading}
+          />
+        </div>
+
+        <button
+          className="btn btn-raised btn-primary"
+          disabled={!email || !password || loading}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
