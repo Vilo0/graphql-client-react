1d4d90b52dae04f30fb1cb444dc571b172aa744c
Edgard Vilo <thevilos@gmail.com>
2021-08-08 12:49:05 -0400

    fix pages in results

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -30,15 +30,17 @@ const PostCard = ({ post, onShow, onShowDelete, onShowUpdate, postModal, email }
         <small>{content}</small>
       </div>
       { (email === post.postedBy.email) && (
-        <div className="row">
-          <div className="col-4">
-            <button onClick={() => handleShow(post)} className="btn btn-primary" type="button">Detalle</button>
-          </div>
-          <div className="col-4">
-            <button onClick={() => handleUpdate(post)} className="btn btn-warning" type="button">Editar</button>
-          </div>
-          <div className="col-4">
-            <button onClick={() => handleDelete(post)} className="btn btn-danger" type="button">Borrar</button>
+        <div className="card-footer">
+          <div className="row">
+            <div className="col-4 text-center m-0 p-0">
+              <button onClick={() => handleShow(post)} className="btn btn-primary" type="button">Detalle</button>
+            </div>
+            <div className="col-4 text-center m-0 p-0">
+              <button onClick={() => handleUpdate(post)} className="btn btn-warning" type="button">Editar</button>
+            </div>
+            <div className="col-4 text-center m-0 p-0">
+              <button onClick={() => handleDelete(post)} className="btn btn-danger" type="button">Borrar</button>
+            </div>
           </div>
         </div>
       )}
--- src/components/modals/PostShow.js
+++ src/components/modals/PostShow.js
@@ -10,7 +10,18 @@ const PostModalShow = ({ show, onClose, postModal }) => {
                     <Modal.Title>Show Post</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>{ JSON.stringify(postModal) }</p>
+                    <img 
+                        src={postModal.image.url}
+                        key={postModal.image.public_id}
+                        alt={postModal.image.public_id}
+                        style={{ height: "100px" }}
+                        className="img-thumbnail mb-4"
+                    />
+                    <p>Content</p>
+                    <p><small>{postModal.content}</small></p>
+                    <br/>
+                    <p>Posted By</p>
+                    <p><small>@{ postModal.postedBy.username } - ({ postModal.postedBy.email })</small></p>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={onClose}>
